0f3e6350ac7fcc17572b1274c2cbbdead5076785
iron768 <iron768@users.noreply.github.com>
2024-01-24 12:34:55 -0500

    Create Test.java
    Add a test java file for check styles

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x=5; // This line violates WhitespaceAround rule
+        System.out.println("Hello, World!");
+    }
+}
