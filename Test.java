be5f03a7ff73ecd5b553180572d440b1025e346d
Nathan G <73437724+ndg8743@users.noreply.github.com>
2024-01-24 12:40:57 -0500

    Update Test.java
    Fixed filename

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Test {
     public static void main(String[] args) {
         int x = 5; // This line violates WhitespaceAround rule
         System.out.println("Hello, World!");
