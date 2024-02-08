2edfc064e3499757a25ff3c3e1f62fbdd10deead
Nathan Gopee <73437724+ndg8743@users.noreply.github.com>
2024-02-08 10:46:04 -0500

    Added Test Folder

--- src/main/java/org/fibsters/InputPayload.java
+++ src/main/java/org/fibsters/InputPayload.java
@@ -3,4 +3,9 @@ package org.fibsters;
 interface InputPayload extends Payload {
     public String getUUID();
     public String getInputType();
+    private String inputType = "defaultInputType";
+    private char delimiter = ',';
+    private String payloadData = "defaultPayloadData";
+    private String outputType = "defaultOutputType";
+    private String outputData = "defaultOutputData";
 }
