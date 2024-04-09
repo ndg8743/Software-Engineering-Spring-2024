efbae8c71c9815da1098b6d89749f8f41f001fef
Nathan Gopee <73437724+ndg8743@users.noreply.github.com>
2024-04-08 22:31:57 -0400

    Added CLIClient: Added the CLI Client that lets user input and converts to json, adds polling, outputs it worked, and saves to file.
    Todo front end stuff:
    Ask user to set file name (ez W)
    Add user input for delimiter/nums

---
+++ src/main/java/org/cliclient/PayloadWrapper.java
@@ -0,0 +1,16 @@
+package org.cliclient;
+
+import org.fibsters.InputPayloadImpl;
+import org.fibsters.interfaces.PayloadData;
+
+class PayloadWrapper {
+    private String uniqueID;
+    private InputPayloadImpl.DirectiveType directive;
+    private PayloadData payloadData;
+
+    public PayloadWrapper(String uniqueID, InputPayloadImpl.DirectiveType directive, PayloadData payloadData) {
+        this.uniqueID = uniqueID;
+        this.directive = directive;
+        this.payloadData = payloadData;
+    }
+}
